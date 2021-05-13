77115a62a1f2f0b1fbc0514efe97d58dffc89902
Nayeli Huerta <nayeli.14.nh@gmail.com>
2021-05-13 12:19:11 -0400

    Agregrar el botón eliminar y opción legendario

--- js/index.js
+++ js/index.js
@@ -17,6 +17,20 @@ tinymce.init({
   
 const listapokemon = [];
 
+const eliminar =  async function(){  //ASYNC VA SIEMPRE QUE SE UTILIZA UNA PROMESA OSEA EL AWAIT
+  let res = await swal.fire({
+    title:`Desea enviar a ${listapokemon[this.nro].nombre} al profesor Oak?`,
+    showCancelButton:true,
+    confirmButtonText:"Sí, enviar."
+  });
+  if (res.isConfirmed){
+    listapokemon.splice(this.nro,1);
+    cargartabla();
+    swal.fire("Pokémon enviado al profesor Oak");
+  }
+  console.log(this.nro);
+}
+
 const cargartabla = ()=>{
   //1. Obtener una referencia de la tabla.
   let tbody = document.querySelector("#tabla-tbody");
@@ -32,35 +46,48 @@ const cargartabla = ()=>{
     tdNumero.innerText = (i+1);
     let tdNombre = document.createElement("td");
     tdNombre.innerText = p.nombre;
+    //CAMBIAR A LENGEDARIO
+    if (p.legendario){
+      tdNombre.classList.add("text-warning");
+    }
     //COLOCAR ICONOS
     let tdTipo = document.createElement("td");
     let icono = document.createElement("i");
     if (p.tipo == "fuego"){
       //<i class="fas fa-fire-alt"></i>
-      icono.classList.add("fas","fa-fire-alt", "text-danger", "fa-3x");
+      icono.classList.add("fas","fa-fire-alt", "text-danger", "fa-2x");
     }else if (p.tipo == "planta"){
       //<i class="fal fa-leaf"></i>
-      icono.classList.add("fas","fa-leaf","text-success", "fa-3x");
+      icono.classList.add("fas","fa-leaf","text-success", "fa-2x");
     }else if (p.tipo == "agua"){
       //<i class="fas fa-tint"></i>
-      icono.classList.add("fas", "fa-tint","text-info", "fa-3x")
+      icono.classList.add("fas", "fa-tint","text-info", "fa-2x")
     }else if (p.tipo == "normal"){
       //<i class="fas fa-bullseye"></i>
-      icono.classList.add("fas", "fa-bullseye", "text-secondary", "fa-3x")
+      icono.classList.add("fas", "fa-bullseye", "text-secondary", "fa-2x")
     }else{
       //<i class="fas fa-bolt"></i>
-      icono.classList.add("fas", "fa-bolt","text-warning", "fa-3x");
+      icono.classList.add("fas", "fa-bolt","text-warning", "fa-2x");
     }
     tdTipo.appendChild(icono)//agregar icono a la tabla
     let tdDescripcion = document.createElement("td");
     tdDescripcion.innerHTML = p.descripcion;
     let tdAcciones = document.createElement("td");
+    tdAcciones.classList.add("text-center");
+    //AGREGAR BOTON PARA ELIMINAR
+    let boton = document.createElement("button");//CREAR ELEMENTO
+    boton.classList.add("btn", "btn-danger"); //CLASES DEL ELEMENTO
+    boton.innerText = "Enviar al profesor Oak"; //CAMBIAR EL TEXTO DE UN ELEMENTO
+    boton.nro = i;
+    boton.addEventListener("click", eliminar);
+    tdAcciones.appendChild(boton); //AGREGAR ELEMENTO DENTRO DE OTRO
 
     //5. Agregar celdas al tr.
     tr.appendChild(tdNumero);
     tr.appendChild(tdNombre);
     tr.appendChild(tdTipo);
     tr.appendChild(tdDescripcion);
+    tr.appendChild(tdAcciones);
     //5. Agregar el tr a la tabla.
     tbody.appendChild(tr);
     //Swal.fire("Bien!","El pokemon ha sido registrado.", "success");
@@ -92,3 +119,10 @@ document.querySelector("#registrar-btn").addEventListener("click",()=>{
 
     //alert("Hola Mundo" + nombre); //Ventana emergente, no recomendable, jode mucho
 } );  
+
+document.querySelector("#limpiar-btn").addEventListener("click", ()=>{
+  document.querySelector("#nombre-txt").value = "";
+  document.querySelector("#legendario-no").checked = true;
+  document.querySelector("#tipo-select").value = "sinValor";
+  tinymce.get("descripcion-txt").setContent(""); //Limpiar el tinymce descripcion
+});
